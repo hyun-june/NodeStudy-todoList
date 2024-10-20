21e4874378cdcaa9daa01616b214910aaaf39f50
Hyunjun Lee <leehyunjune11@gmail.com>
2024-10-20 22:00:42 +0900

    [241020] deploy test

--- todoList-frontend/src/App.js
+++ todoList-frontend/src/App.js
@@ -8,7 +8,7 @@ import RegisterPage from "./pages/RegisterPage";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<TodoPage />} />
+      <Route path="/" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
 
       <Route path="/login" element={<LoginPage />} />

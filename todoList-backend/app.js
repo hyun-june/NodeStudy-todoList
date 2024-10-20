09ab1608327d2d905755ff8e0e932e9e5d6c36e8
Hyunjun Lee <leehyunjune11@gmail.com>
2024-10-20 17:11:28 +0900

    [241020] login backend

--- todoList-backend/app.js
+++ todoList-backend/app.js
@@ -1,20 +1,3 @@
-// 할일 앱
-
-// restful APi
-// 주소 +http 명령어
-
-// 1. 할일을 추가 할 수 있다. Create /tasks post
-// 2. 할일 리스트를 볼 수 있다. Read /tasks get
-// 3. 할일에 대해서 상태를 표시 할 수 있다. Update /tasks/:id put
-// 4. 할일을 삭제 할 수 있다. Delete /tasks/:id delete
-
-// 백엔드 준비
-// 1. 기본 세팅 : npm,express,mongoose,app 리스너
-// 2. 라우터 주소 정의
-// 3. 데이터베이스 스키마 정의
-// 4. 기능정의 CRUD
-// 5. 테스트 : 포스트맨
-
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
--- todoList-backend/controller/task.controller.js
+++ todoList-backend/controller/task.controller.js
@@ -7,8 +7,8 @@ taskController.createTask = async (req, res) => {
     const newTask = new Task({ task, isComplete });
     await newTask.save();
     res.status(200).json({ status: "ok", data: newTask });
-  } catch (err) {
-    res.status(400).json({ status: "fail", error: err.message });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error });
   }
 };
 
@@ -17,7 +17,7 @@ taskController.getTask = async (req, res) => {
     const taskList = await Task.find({}).select("-__v");
     res.status(200).json({ status: "ok", data: taskList });
   } catch (err) {
-    res.status(400).json({ status: "fail", error: err.message });
+    res.status(400).json({ status: "fail", error });
   }
 };
 
@@ -26,7 +26,7 @@ taskController.updateTask = async (req, res) => {
     const updateTask = await Task.findByIdAndUpdate(req.params.id, req.body);
     res.status(200).json({ status: "ok", data: updateTask });
   } catch (err) {
-    res.status(400).json({ status: "fail", error: err.message });
+    res.status(400).json({ status: "fail", error });
   }
 };
 
